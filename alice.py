675d7a42a47a372cbfdd13658e62e859c4cc3683
Piyush Udhao <pudhao12@gmail.com>
2025-11-05 13:29:44 +0530

    game works

--- alice.py
+++ alice.py
@@ -18,13 +18,13 @@ broadcast_thread.start()
 print("============================================= Game Start (Alice) =============================================")
 
 (n, eA, dA) = rsa.generate_alice_keys()
-print(f"[INFO]\tAlice's Keys: n={n}, eA={eA}, dA={dA}")
+print(f"[INFO]\t\t\tAlice's Keys: n= {n}, public key =  {eA}, private key = {dA}")
 
-print("\n[ENCRYPTION]\tEncrypting 'heads' and 'tails' with Alice's public key.")
+print("\n[ENCRYPTION]\t\tEncrypting 'heads' and 'tails' with Alice's public key.")
 c_heads = rsa.encrypt(MSG_HEADS, eA, n)
 c_tails = rsa.encrypt(MSG_TAILS, eA, n)
-print(f"[INFO] C_HEADS = {c_heads}")
-print(f"[INFO] C_TAILS = {c_tails}")
+print(f"[INFO]\t\t\tEncrypted HEADS = {c_heads}")
+print(f"[INFO]\t\t\tEncrypted TAILS = {c_tails}")
 alice_copy = {c_heads: MSG_HEADS, c_tails: MSG_TAILS}
 
 conn, server_socket = network.create_server()
@@ -37,20 +37,19 @@ with conn:
     bob_packet_str, bob_prediction = data.split(':')
     bob_packet_doubly_encrypted = int(bob_packet_str)
 
-    print(f"\n[NETWORK]\tReceived packet from Bob: {bob_packet_doubly_encrypted}")
-    print(f"[GAME]\tBob's prediction: {bob_prediction}")
+    print(f"\n[NETWORK]\t\tReceived packet from Bob: {bob_packet_doubly_encrypted}")
+    print(f"[GAME]\t\t\tBob's prediction: {bob_prediction}")
 
-    print("[DECRYPTION]\tDecrypting packet with my private key (dA)...")
+    print("[DECRYPTION]\t\tDecrypting packet with Alice's private key.")
     half_decrypted_packet = rsa.decrypt(bob_packet_doubly_encrypted, dA, n)
-    print(f"[NETWORK]\tSending half-decrypted packet: {half_decrypted_packet}")
+    print(f"[NETWORK]\t\tSending half-decrypted packet: {half_decrypted_packet}")
     network.send_message(conn, str(half_decrypted_packet))
 
     bob_key_verification = network.receive_message(conn)
-    print(f"\n[GAME]\tReceived Bob's keys for verification: {bob_key_verification}")
-
     _, eB_str, dB_str = bob_key_verification.split(":")
     eB = int(eB_str)
     dB = int(dB_str)
+    print(f"\n[GAME]\t\t\tReceived Bob's keys for verification: public key = {eB}, private key = {dB}")
 
     original_message = rsa.decrypt(half_decrypted_packet, dB, n)
 
@@ -63,17 +62,17 @@ with conn:
     else:
         print("VERIFICATION FAILED: Unknown message!")
 
-    print(f"[INFO]\tBob's half-decrypted packet decrypted with his key is: {original_message}")
-    print(f"[INFO]\tThis corresponds to: {original_choice}")
+    print(f"[INFO]\t\t\tBob's half-decrypted packet decrypted with his key is: {original_message}")
+    print(f"[INFO]\t\t\tThis corresponds to: {original_choice}")
 
     if bob_prediction == original_choice:
-        print(f"[GAME]\tBob's prediction ({bob_prediction}) was CORRECT.")
-        print("[WINNER]\tBOB WINS")
+        print(f"[GAME]\t\t\tBob's prediction ({bob_prediction}) was CORRECT.")
+        print("[WINNER]\t\tBOB WINS")
     else:
-        print(f"[GAME]\tBob's prediction ({bob_prediction}) was INCORRECT.")
-        print("[WINNER]\tALICE WINS")
+        print(f"[GAME]\t\t\tBob's prediction ({bob_prediction}) was INCORRECT.")
+        print("[WINNER]\t\tALICE WINS")
 
 # Clean up the main server socket
 server_socket.close()
-print("\n[NETWORK]\tServer shut down.")
+print("\n[NETWORK]\t\tServer shut down.")
 print("============================================= Game End (Alice) =============================================")
--- bob.py
+++ bob.py
@@ -10,15 +10,15 @@ MSG_TAILS = 22222
 # Server discovery.
 server_ip = discovery.find_server_ip()
 if not server_ip:
-    print("[NETWORK]\tCould not find Alice on the network. Exiting.")
+    print("[NETWORK]\t\tCould not find Alice on the network. Exiting.")
     sys.exit()
 
 print("============================================= Game Start (Bob) =============================================")
 
 (n, eB, dB) = rsa.generate_bob_keys()
-print(f"Bob's Keys: n={n}, e={eB}, d={dB}")
+print(f"Bob's Keys: n= {n}, public key = {eB}, private key = {dB}")
 
-conn = network.connect_to_server()
+conn = network.connect_to_server(server_ip)
 
 if not conn:
     sys.exit()
@@ -28,33 +28,32 @@ with conn:
     c_heads_str, c_tails_str = packet_data.split(':')
     c_heads = int(c_heads_str)
     c_tails = int(c_tails_str)
-    print(f"[NETWORK]\tReceived encrypted packets from Alice:\n1: {c_heads}\n2: {c_tails}")
+    print(f"[NETWORK]\t\tReceived encrypted packets from Alice: \t{c_heads}\t\t{c_tails}")
 
     # Pick one packet randomly.
     packets = [c_heads, c_tails]
     picked_packet = random.choice(packets)
-    print(f"\n[INFO]\tRandomly picked packet: {picked_packet}")
+    print(f"\n[INFO]\t\t\tRandomly picked a packet!")
 
     # User makes prediction.
     prediction = ""
     while prediction not in ["heads", "tails"]:
-        prediction = input("[USER INPUT] Make your prediction (heads/tails): ").lower().strip()
+        prediction = input("[USER INPUT]\t\tBob's prediction (heads/tails): ").lower().strip()
 
-    print("[ENCRYPTION]\tEncrypting the packet user picked with Bob's public key.")
+    print("[ENCRYPTION]\t\tEncrypting the randomly picked packet Bob's public key.")
     doubly_encrypted_packet = rsa.encrypt(picked_packet, eB, n)
 
-    # Send packet and prediction to Alice
     payload = f"{doubly_encrypted_packet}:{prediction}"
-    print(f"[NETWORK]\tSending doubly-encrypted packet and prediction to Alice.")
+    print(f"[NETWORK]\t\tSending doubly-encrypted packet and prediction to Alice.")
     network.send_message(conn, payload)
 
     half_decrypted_str = network.receive_message(conn)
     half_decrypted_packet = int(half_decrypted_str)
-    print(f"\n[NETWORK]\tReceived half-decrypted packet from Alice: {half_decrypted_packet}")
+    print(f"\n[NETWORK]\t\tReceived half-decrypted packet from Alice: {half_decrypted_packet}")
 
-    print("[DECRYPTION]\tDecrypting packet with Bob's private key.")
+    print("[DECRYPTION]\t\tDecrypting packet with Bob's private key.")
     original_message = rsa.decrypt(half_decrypted_packet, dB, n)
-    print(f"[INFO]\tDecrypted Packet: {original_message}")
+    print(f"[INFO]\t\t\tDecrypted Packet: {original_message}")
 
     original_choice = ""
     if original_message == MSG_HEADS:
@@ -62,17 +61,17 @@ with conn:
     elif original_message == MSG_TAILS:
         original_choice = "tails"
 
-    print("\n[NETWORK]\tSending Bob's public and private keys to Alice for verification.")
+    print("\n[NETWORK]\t\tSending Bob's public and private keys to Alice for verification.")
     key_payload = f"{n}:{eB}:{dB}"
     network.send_message(conn, key_payload)
 
-    print(f"[GAME]\tDecrypted Packet: {original_choice}")
-    print(f"[GAME]\tBob's Prediction: {prediction}")
+    print(f"[GAME]\t\t\tDecrypted Packet: {original_choice}")
+    print(f"[GAME]\t\t\tBob's Prediction: {prediction}")
 
     if prediction == original_choice:
-        print("[WINNER]\tBob wins!")
+        print("[WINNER]\t\tBob wins!")
     else:
-        print("[WINNER]\tAlice wins!")
+        print("[WINNER]\t\tAlice wins!")
 
-print("[NETWORK]\tConnection closed.")
+print("[NETWORK]\t\tConnection closed.")
 print("============================================= Game End (Bob) =============================================")

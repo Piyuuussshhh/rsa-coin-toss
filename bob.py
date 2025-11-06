91bb361ea5ff4c25d9edf3fbe459011fd657e80e
Piyush Udhao <pudhao12@gmail.com>
2025-11-06 09:51:22 +0530

    made stupid changes

--- bob.py
+++ bob.py
@@ -13,27 +13,36 @@ if not server_ip:
     print("[NETWORK]\t\tCould not find Alice on the network. Exiting.")
     sys.exit()
 
+conn = network.connect_to_server(server_ip)
+if not conn:
+    sys.exit()
+print("Would you like to connect to the server (Alice) (y/n): ")
+n = input()
+
+print("Connection established! Would you like to start the game? (y/n): ")
+n = input()
+if n == "y":
+    print("Great! Requested Alice for heads and tails packets!")
+
 print("============================================= Game Start (Bob) =============================================")
 
 (n, eB, dB) = rsa.generate_bob_keys()
-print(f"Bob's Keys: n= {n}, public key = {eB}, private key = {dB}")
-
-conn = network.connect_to_server(server_ip)
-
-if not conn:
-    sys.exit()
+print(f"Bob's Generated Keys: n= {n}, public key = {eB}, private key = {dB}")
 
 with conn:
     packet_data = network.receive_message(conn)
+    print("[GAME]\t\tReceived encrypted heads and tails from Alice!")
     c_heads_str, c_tails_str = packet_data.split(':')
     c_heads = int(c_heads_str)
     c_tails = int(c_tails_str)
     print(f"[NETWORK]\t\tReceived encrypted packets from Alice: \t{c_heads}\t\t{c_tails}")
 
     # Pick one packet randomly.
+    print("[GAME]\t\tPick a packet (1/2): ")
+    n = int(input())
     packets = [c_heads, c_tails]
-    picked_packet = random.choice(packets)
-    print(f"\n[INFO]\t\t\tRandomly picked a packet!")
+    picked_packet = packets[n - 1]
+    print(f"[GAME]\t\tUser picked the packet {picked_packet}")
 
     # User makes prediction.
     prediction = ""
